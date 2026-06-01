d29cddb9540b0a9dc7cd6227de9f0d254c994104
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-31 19:52:38 -0500

    Clean up while loop syntax in ViewINI-v2.sh
    Removed unnecessary 'done' statements from the while loops in the INI list functions.

--- ViewINI-v2.sh
+++ ViewINI-v2.sh
@@ -143,16 +143,17 @@ echo "${brightyellow}Available sections: ${reset}"
 echo "${brightyellow}-------------------${reset}"
 echo " "
 
+#######################################################################################
+# INI List Functions #																  #
+#######################################################################################
 
 ini_list_sections "$CONFIG_DIR/$CONFIG_FILE" | while read section; do
        echo "${brightblue} [$section] ${reset}"
-	   done
-
+	   
 ini_list_keys "$CONFIG_DIR/$CONFIG_FILE" "$section" | while read key; do
        value=$(ini_read "$CONFIG_DIR/$CONFIG_FILE" "$section" "$key")
        echo "  - ${brightyellow} $key ${brightred} = ${brightwhite} $value ${reset}"
-	   done
-
+	
 
 
 # Display Output #
